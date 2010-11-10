a4bcb4c7d35130edfbc4796a3f0fdb42c057d156
mrsarm@gmail.com <mrsarm@gmail.com@2d82698d-7f3c-5550-4557-938e166d3f7f>
2010-11-10 15:17:27 +0000

    Borro tageos mal echos

--- modules/mr-common-dao/tags/mr-common-dao-1.0.0/util/sql/dropschemascript-mysql.sql
+++
@@ -1,7 +0,0 @@
-/*
- *  Script de borrado del tablaspace y del usuario/esquema usado en los test
- *  con la BBDD MySQL.
- */
-
-DROP USER admincommon;
-DROP DATABASE /*! IF EXISTS */ test_mr_common;
--- modules/mr-common-web/tags/mr-common-web-1.0.0/util/jsp/default.jsp
+++
@@ -1,3 +0,0 @@
-<%@ page contentType="application/json; charset=UTF-8" %>
-<%@ taglib prefix="fwk" tagdir="/WEB-INF/tags/fwk" %>
-<fwk:json></fwk:json>
