d4518c4eafcf58683ae02e1b198bb570882e378f
mrsarm@gmail.com <mrsarm@gmail.com@2d82698d-7f3c-5550-4557-938e166d3f7f>
2011-01-07 21:46:58 +0000

    Comparador base configurable y ordenable, y comparadores para objetos OptionData

---
+++ modules/mr-common-core/trunk/src/main/java/mr/common/model/comparator/OptionDataByCodeComparator.java
@@ -0,0 +1,35 @@
+package mr.common.model.comparator;
+
+import mr.common.model.OptionData;
+
+
+/**
+ * Comparador que determina la comparación con el valor
+ * de {@link mr.common.model.OptionData#getCode() OptionData.getCode()}
+ * de ambos objetos, ignarando si son may/minus.
+ *
+ * @see mr.common.model.comparator.ConfigurableComparator
+ * @author Mariano Ruiz
+ */
+public class OptionDataByCodeComparator extends ConfigurableComparator<OptionData> {
+
+	/**
+	 * @see mr.common.model.comparator.ConfigurableComparator
+	 * #ConfigurableComparator()
+	 */
+	public OptionDataByCodeComparator() {
+		super();
+	}
+
+	/**
+	 * @see mr.common.model.comparator.ConfigurableComparator
+	 * #ConfigurableComparator(int)
+	 */
+	public OptionDataByCodeComparator(int order) {
+		super(order);
+	}
+
+	public int compare(OptionData o1, OptionData o2) {
+		return getOrder() * o1.getCode().compareToIgnoreCase(o2.getCode());
+	}
+}
---
+++ modules/mr-common-core/trunk/src/main/java/mr/common/model/comparator/OptionDataByDescriptionComparator.java
@@ -0,0 +1,35 @@
+package mr.common.model.comparator;
+
+import mr.common.model.OptionData;
+
+
+/**
+ * Comparador que determina la comparación con el valor
+ * de {@link mr.common.model.OptionData#getDescription() OptionData.getDescription()}
+ * de ambos objetos, ignarando si son may/minus.
+ *
+ * @see mr.common.model.comparator.ConfigurableComparator
+ * @author Mariano Ruiz
+ */
+public class OptionDataByDescriptionComparator extends ConfigurableComparator<OptionData> {
+
+	/**
+	 * @see mr.common.model.comparator.ConfigurableComparator
+	 * #ConfigurableComparator()
+	 */
+	public OptionDataByDescriptionComparator() {
+		super();
+	}
+
+	/**
+	 * @see mr.common.model.comparator.ConfigurableComparator
+	 * #ConfigurableComparator(int)
+	 */
+	public OptionDataByDescriptionComparator(int order) {
+		super(order);
+	}
+
+	public int compare(OptionData o1, OptionData o2) {
+		return getOrder() * o1.getDescription().compareToIgnoreCase(o2.getDescription());
+	}
+}
