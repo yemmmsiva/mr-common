3c51dcfaef7c6baae3fe7306ab5e6824ad38ed68
mrsarm@gmail.com <mrsarm@gmail.com@2d82698d-7f3c-5550-4557-938e166d3f7f>
2011-04-08 19:30:58 +0000

    Test de validación del XSD de usuarios

--- modules/mr-common-security/trunk/src/test/java/mr/common/security/test/xml/UserXmlTest.java
+++ modules/mr-common-security/trunk/src/test/java/mr/common/security/test/xml/UserXmlTest.java
@@ -8,16 +8,23 @@ import javax.xml.parsers.ParserConfigurationException;
 import mr.common.format.XmlUtils;
 
 import org.junit.Test;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 
 public class UserXmlTest {
 
+	File xmlUsers = new File(getTestPrefixResourcePath() + "users.xml");
+	File xsdUsers = new File(getMainPrefixResourcePath() + "schema/users-1.0.xsd");
+
 	@Test
-	public void testValidateXML() throws ParserConfigurationException, SAXException, IOException {
-		File xmlUsers = new File(getTestPrefixResourcePath() + "users.xml");
-		File xsdUsers = new File(getMainPrefixResourcePath() + "schema/users-1.0.xsd");
+	public void testValidateXml() throws ParserConfigurationException, SAXException, IOException {
 		XmlUtils.getAndValidateDocument(xmlUsers, xsdUsers);
+	}
+
+	@Test
+	public void testXmlUsers() throws ParserConfigurationException, SAXException, IOException {
+		Document dom = XmlUtils.getDocument(xmlUsers);
 	}
 
 	private String getTestPrefixResourcePath() {
