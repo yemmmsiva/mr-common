58822bd10e4ecc76b2904f23a411a4ac73750235
mrsarm@gmail.com <mrsarm@gmail.com@2d82698d-7f3c-5550-4557-938e166d3f7f>
2011-05-09 05:50:04 +0000

    Agrego al password validator validación a la longitud de la contraseña

--- modules/mr-common-security-userentity/trunk/src/main/java/mr/common/security/userentity/validator/PasswordValidator.java
+++ modules/mr-common-security-userentity/trunk/src/main/java/mr/common/security/userentity/validator/PasswordValidator.java
@@ -16,11 +16,38 @@ public class PasswordValidator extends Validator {
 
 	private static final long serialVersionUID = 1L;
 
+	private int minLength = 4;
+	private int maxLength = 32;
+
+	public PasswordValidator() { }
+
+	public PasswordValidator(int minLength, int maxLength) {
+		if(minLength<1) {
+			throw new IllegalArgumentException("minLength < 1.");
+		} else if(minLength>32) {
+			throw new IllegalArgumentException("minLength > 32.");
+		}
+		if(maxLength<1) {
+			throw new IllegalArgumentException("maxLength < 1.");
+		} else if(maxLength>32) {
+			throw new IllegalArgumentException("maxLength > 32.");
+		}
+		if(minLength>maxLength) {
+			throw new IllegalArgumentException("minLength > maxLength.");
+		}
+		this.minLength = minLength;
+		this.maxLength = maxLength;
+	}
+
 	public boolean isValid(Object password) {
-		if(!StringUtils.hasText((String)password)) {
+		String pass = (String) password;
+		if(!StringUtils.hasText(pass)) {
 			return false;
 		}
-		return ValidatorUtils.isValidUsername((String)password);
+		if(pass.length()<minLength || pass.length()>maxLength) {
+			return false;
+		}
+		return ValidatorUtils.isValidUsername(pass);
 	}
 
 	/**
