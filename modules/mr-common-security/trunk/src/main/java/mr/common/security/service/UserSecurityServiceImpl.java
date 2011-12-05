f151cc7cc2ebcd8008a7514684883fd7fed4acab
mrsarm@gmail.com <mrsarm@gmail.com@2d82698d-7f3c-5550-4557-938e166d3f7f>
2011-12-05 00:49:21 +0000

    Add setter and constructor arg to change the username to UserSecurityServiceImpl

--- modules/mr-common-security/trunk/src/main/java/mr/common/security/service/UserSecurityServiceImpl.java
+++ modules/mr-common-security/trunk/src/main/java/mr/common/security/service/UserSecurityServiceImpl.java
@@ -3,19 +3,36 @@ package mr.common.security.service;
 
 /**
  * Implementación de {@link mr.common.security.service.UserSecurityService
- * UserSecurityService} que devuelve siempre la constante
- * {@link mr.common.security.service.UserSecurityService#APP_USER APP_USER}
- * en el método {@link #getCurrentUsername()}.<br/>
+ * UserSecurityService} que devuelve como username el valor
+ * retornado por {@link #getCurrentUsername()}, que puede ser
+ * configurado por {@link #setCurrentUsername(String)}.<br/>
  * Usar esta implementación en sistemas que no soportan sesiones con
  * usuarios, pero por razones de auditoría u otra necesitan de
- * un usuario "responsable".
+ * un usuario "responsable" o que identifique a la aplicación.
  * 
  * @see mr.common.security.service.UserSecurityService
  * @author Mariano Ruiz
  */
 public class UserSecurityServiceImpl implements UserSecurityService {
 
+	private String currentUsername = APP_USER;
+
+	public UserSecurityServiceImpl() { }
+
+	public UserSecurityServiceImpl(String currentUsername) {
+		this.currentUsername = currentUsername;
+	}
+
+	/**
+	 * Devuelve el nombre de usuario, por default
+	 * si no es configurado retorna {@link
+	 * mr.common.security.service.UserSecurityService#APP_USER}
+	 */
 	public String getCurrentUsername() {
-		return APP_USER;
+		return this.currentUsername;
+	}
+
+	public void setCurrentUsername(String currentUsername) {
+		this.currentUsername = currentUsername;
 	}
 }
